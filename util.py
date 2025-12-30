d918352fbbcc7574af6fc2744b7f9fbc934c3d6f
Shreyoshi Ghosh <shreyoshighosh2005@gmail.com>
2025-12-30 20:47:10 +0530

    Add HSV color limit utility function

--- util.py
+++ util.py
@@ -1,2 +1,11 @@
 import numpy as np
+import cv2
 
+def get_limits(color):
+    c=np.uint8([[color]])
+    hsvC=cv2.cvtColor(c,cv2.COLOR_BGR2HSV)
+
+    lower_limit=np.array([hsvC[0][0][0]-10,100,100])
+    upper_limit=np.array([hsvC[0][0][0]+10,255,255])
+
+    return lower_limit,upper_limit
